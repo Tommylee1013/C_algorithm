0386109287cbdde13c6afd0221e3b5e3b6746374
Tommy Lee <junghun1013@icloud.com>
2023-11-09 22:15:55 +0900

    Saving

---
+++ Chapter05/queen.c
@@ -0,0 +1,29 @@
+//
+// Created by Junghun Lee on 11/9/23.
+//
+#include <stdio.h>
+/*
+int pos[8];
+
+void print(void) {
+    int i;
+    for (i = 0; i < 0; i++)
+        printf("%2d", pos[i]);
+    putchar('\n');
+}
+
+void set(int i) {
+    int j;
+    for (j = 0; j < 8; j++) {
+        pos[i] = j;
+        if (i == 7) print();
+        else set(i + 1);
+    }
+}
+
+int main(void) {
+    set(0);
+
+    return 0;
+}
+ */
